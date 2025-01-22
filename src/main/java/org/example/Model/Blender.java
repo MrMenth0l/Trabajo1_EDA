17e21c9753c6491ab39936fb92a79ff53c6cbf21
yehosuahercules <yehosua.hercules4c@gmail.com>
2025-01-22 11:40:44 -0600

    small fix, actual capacity now float

---
+++ src/main/java/org/example/Model/Blender.java
@@ -0,0 +1,82 @@
+package org.example.Model;
+
+class Blender implements IBlender {
+    private boolean powerStatus = false;
+    private byte speed = 0;
+    private float currentCapacity = 0;
+    private final float maxCapacity = 2000; // in milliliters
+
+    @Override
+    public byte checkPowerStatus() {
+        return (byte) (powerStatus ? 1 : 0);
+    }
+
+    @Override
+    public byte switchPowerStatus() {
+        powerStatus = !powerStatus;
+        return checkPowerStatus();
+    }
+
+    @Override
+    public void fillBlender(String prod, float ml) {
+        if (currentCapacity + ml > maxCapacity) {
+            currentCapacity = currentCapacity;
+        } else {
+            currentCapacity += ml;
+        }
+    }
+
+    @Override
+    public void fillBlender(String prod) {
+        fillBlender(prod, 300); // Default to 500ml
+    }
+
+    @Override
+    public float actualCapacity() {
+        return (float) currentCapacity;
+    }
+
+    @Override
+    public void increaseSpeed() {
+        if (!powerStatus) {
+            return;
+        }
+        if (speed < 10) {
+            speed++;
+        }
+    }
+
+    @Override
+    public void decreaseSpeed() {
+        if (!powerStatus) {
+            return;
+        }
+        if (speed > 0) {
+            speed--;
+        }
+    }
+
+    @Override
+    public byte checkSpeed() {
+        return speed;
+    }
+
+    @Override
+    public boolean isFull() {
+        return currentCapacity >= maxCapacity;
+    }
+
+    @Override
+    public void emptyBlender() {
+        currentCapacity = 0;
+    }
+
+    @Override
+    public void emptyBlender(float ml) {
+        if (ml > currentCapacity) {
+            emptyBlender();
+        } else {
+            currentCapacity -= ml;
+        }
+    }
+}
--- src/test/java/org/example/View/AppTest.java
+++ src/test/java/org/example/View/AppTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.View;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
