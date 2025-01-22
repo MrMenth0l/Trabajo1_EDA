37204dd81695c93876eeba8602cc35e5dd1816b5
yehosuahercules <yehosua.hercules4c@gmail.com>
2025-01-22 11:15:19 -0600

    interfacelicuadora

--- src/main/java/org/example/Model/ILicuadora.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public interface ILicuadora {
-    void encender();
-    int llenar(String ingrediente);
-    void aumentarVelocidad();
-    void disminuirVelocidad();
-    
-}
